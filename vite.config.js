987e75411b5923eec51f364e7a3922898d8d3abb
WalterNickSuelArroyo <nicodehero21@outlook.com>
2025-01-21 18:24:54 -0500

    actualizacion

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,14 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/proyecto-listar-tareas/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: 'build',
   },
 })
